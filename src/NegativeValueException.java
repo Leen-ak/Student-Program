7cac8ae09a09d471ebb60e833a7a4f0108623921
Leen Aboukhalil <109052421+Leen-ak@users.noreply.github.com>
2024-06-01 18:58:02 -0400

    Update NegativeValueException.java

--- src/NegativeValueException.java
+++ src/NegativeValueException.java
@@ -1,8 +1,9 @@
  /**
   * Program Name: NegativeValueException.java
-  * Purpose: OK DUMMY, PUT SOMETHING DESCRIPTIVE HERE!
-  * @author Your Name and Student Number goes here
-  * Date: May 13, 2024
+  * Purpose: NegativeValueException class is to define a custom exception for handling invalid grade values, specifically when a grade is negative or 
+  	outside the valid range of 0 to 100. This exception includes a descriptive error message and an invalid grade value.
+  * @author Leen Aboukhalil
+  * Date: May 27, 2024
  */
 
 public class NegativeValueException extends Exception{
